639010fb6acb6e57796617aa63a2a74da473dcec
Nick Farni <nickfarni001@gmail.com>
2020-01-08 17:28:27 -0700

    unable to load next question

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
+console.log('Top of the JS');
+
 //question database
 const STORE = [{
     card_title: 'Ready to Pineapple?',
-    card_text: 'Your score is currently negative ten points. Did you think I was going to start you off with zero points? What sort of pinapple do you think I am?',
+    card_text: 'You currently have negative ten points. Did you think I was going to start you off with zero points? What sort of pinapple do you think I am?',
     answer_1: 'You\'re a terrible pineapple (said on opposite day)!',
     answer_2: 'I hate you (when \'I hate you\' means \' I love your infinite wisdom — it\' truly magisterial\')!',
     answer_3: 'Start the quiz',
     correct_answer_title: 'Great job!',
-    correct_answer_text: 'All three of those were correct. I will bump you up to zero points. Now you have nothing. Which was better than what you had before. You are welcome',
-    correct_answer_button_1: 'Next question',
+    correct_answer_text: 'All three of those were correct. I will bump you up to zero points. Now you have nothing. Which was more than you had before. You\'re welcome.',
     replay_card_title: 'Wow, you\'re back?',
     replay_card_text: 'I didn\'t think you\'d actually play again. Well ... I\'m still starting you off with negative ten points. You don\'t get special treatment just because you done pineappled before.'
   },
@@ -20,10 +21,8 @@ const STORE = [{
     the_correct_answer_is: 'regenerate',
     correct_answer_title: 'Correct!',
     correct_answer_text: 'You can plant pineapple leaves to grow a new plant.',
-    correct_answer_button_1: 'Next question',
     wrong_answer_title: 'Absolutely not!',
     wrong_answer_text: 'Your choice was incredibly stupid, my apologies.',
-    correct_answer_button_1: 'I\'ll try better next time!'
   },
   {
     card_title: 'Question 2',
@@ -90,70 +89,151 @@ const STORE = [{
   }
 ];
 
+// User score starts at negative ten
 let theScore = -10;
 let questionNumber = 0;
 let timesPlayed = 0
+let questionsAnsweredCorrectly = 0;
 
-// Run this to make the HTML code
-function makeTheQuestionHTML(){ 
-  let questionHTML =  $(`
+function makeQuestionHTML() {
+  return $(`
   <form>
   <fieldset>
-    <legend><h1>Are you ready to pineapple?</h1></legend>
-    <p>That's pineapple code for "I'm ready to pineapple." It's a pretty intuitive language. This challange will be tough. It will certainly be about pineapples. You can definitely count on that. Ready to begin?</p>
-    <button type="submit" form="form" value="correct">Skip (Not allowed)</button>
-    <button type="submit" form="form" value="correct">No (Disabled)</button>
-    <button type="submit" form="form" value="correct">Yes</button>
+    <legend><h1>${STORE[questionNumber].card_title}</h1></legend>
+    <div>Your current score is: <span class="current-score">${theScore}</span></div>
+    <p>${STORE[questionNumber].card_text}</p>
+    <button type="submit" form="form" class="button" value="${STORE[questionNumber].answer_1}">${STORE[questionNumber].answer_1}</button>
+    <button type="submit" form="form" class="button" value="${STORE[questionNumber].answer_2}">${STORE[questionNumber].answer_2}</button>
+    <button type="submit" form="form" class="button" value="${STORE[questionNumber].answer_3}">${STORE[questionNumber].answer_3}</button>
+  </fieldset>
+</form>
+  `)
+}
+
+// Run this to make the HTML code
+function makeTheFirstQuestionHTML() {
+  let questionHTML = $(`
+  <form>
+  <fieldset>
+    <legend><h1>${STORE[questionNumber].card_title}</h1></legend>
+    <div>Your current score is: <span class="current-score">${theScore}</span></div>
+    <p>${STORE[questionNumber].card_text}</p>
+    <button type="submit" form="form" value="${STORE[questionNumber].answer_1}">${STORE[questionNumber].answer_1}</button>
+    <button type="submit" form="form" value="${STORE[questionNumber].answer_1}">${STORE[questionNumber].answer_2}</button>
+    <button type="submit" form="form" value="${STORE[questionNumber].answer_1}">${STORE[questionNumber].answer_3}</button>
   </fieldset>
 </form>
   `)
   return questionHTML
 }
 
+function updateScore(rightOrWrong) {
+  if (rightOrWrong === 'right') {
+    console.log('You answered correctly!');
+  theScore += 10;
+  $('.current-score').text(theScore);
+} else {
+  console.log('You answered correctly!');
+  theScore -= 10;
+  $('.current-score').text(theScore);
+}
+}
+
+function updateQuestionNumber() {
+  questionNumber++;
+}
+
 // Checks to see if this is the first time the user has played
 // and, if so, runs a function to create the HTML
 function generateQuestion() {
-  if (timesPlayed === 0) { 
-    
-    return makeTheQuestionHTML()
-
+  if (timesPlayed === 0 && questionNumber !== 0) {
+    console.log('generateQuestion ran');
+    return makeTheFirstQuestionHTML()
+  } else {
+    console.log('generate a non-zero question');
+    return makeQuestionHTML()
   }
 }
 
 // Run this first to load in the first question
 function initializeQuiz() {
   if (questionNumber === 0) {
-    $('.main-area').append(generateQuestion())
+    console.log('initializeQuiz ran');
+    $('.main-area').append(generateQuestion());
+  } else if (questionNumber !== 0) {
+    $('.main-area').append(generateQuestion());
   }
 }
 
 // Do this when the user gets the right answer
 function correctAnswer() {
-  $(`CORRECT ANSWER!`).append($('.main-area'));
+  console.log('correctAnswer ran');
+  $('form').hide();
+  $('.main-area').html(`<form>
+    <fieldset>
+      <legend><h1>${STORE[questionNumber].correct_answer_title}</h1></legend>
+      <div>Your current score is: <span class="current-score">${theScore}</span></div>
+      <p>${STORE[questionNumber].correct_answer_text}</p>
+      <button type="button" class="nextButton">Next Question</button>
+    </fieldset>
+  </form>`)
+  updateScore('correct');
 }
 
+function wrongAnswer() {
+  $('form').hide();
+  $('.main-area').html(`
+  <form>
+    <fieldset>
+      <legend><h1>${STORE[questionNumber].wrong_answer_title}</h1></legend>
+      <div>Your current score is: <span class="current-score">${theScore}</span></div>
+      <p>${STORE[questionNumber].wrong_answer_text}</p>
+      <button type="button" class="nextButton">Next Question</button>
+    </fieldset>
+  </form>
+      `)
+  updateScore('incorrect');
+};
+
 // When a user selects one of the answer buttons
-function submitAnswer(){
-  $('.announce-card').on('submit'), function(event) {
+function submitAnswer() {
+  $('.announce-card').on('click', 'button', function (event) {
     event.preventDefault();
-    let selected = (event.currentTarget).val();
+    let selected = event.currentTarget.value;
     let correct = STORE[questionNumber].the_correct_answer_is;
-    if (questionNumber === 0){
+    console.log(`
+      submitAnswer - The value of the selected button is: ${selected}
+      `);
+    if (questionNumber === 0) {
+      console.log(`The question number is :${questionNumber} and should be 0. CORRECT ANSWER will run.`);
       correctAnswer();
     } else if (questionNumber !== 0) {
-      if (answer === correct) {
+      if (selected === correct) {
+        console.log(`The question number is :${questionNumber}. selected equals correct!`);
         correctAnswer();
       } else {
+        console.log(`The question number is :${questionNumber}. selected equals wrong!`);
         wrongAnswer();
       }
     }
-  }
+  });
 }
+
+function nextQuestion() {
+  $('.annouce-card').on('click', '.nextButton', function (event) {
+    $('form').hide();
+    console.log('nextQuestion ran!');
+    updateQuestionNumber();
+    initializeQuiz()
+  })
+};
 
 
 // This runs all the functions
 function makeTheQuiz() {
   initializeQuiz();
+  generateQuestion()
+  submitAnswer()
 }
 
 
